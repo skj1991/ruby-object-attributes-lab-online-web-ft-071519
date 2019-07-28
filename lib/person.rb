c8fca8614218b0fa0d92ad3f597dff008847eab2
Stacy Johns <stacykjohns@icloud.com>
2019-07-28 20:10:18 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name=(persons_name)
+    @the_persons_name = persons_name
+  end
+  def name
+    @the_persons_name
+  end
+  def job=(persons_job)
+    @the_persons_job = persons_job
+  end
+  def name
+    @the_persons_job
+  end
+end
