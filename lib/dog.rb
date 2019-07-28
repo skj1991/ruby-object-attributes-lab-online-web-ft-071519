d9e0a31d7519b934378dc6ad738aebc461a8f6cf
Stacy Johns <stacykjohns@icloud.com>
2019-07-28 20:08:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,14 @@
 class Dog
+  def name=(dog_name)
+    @the_dog_name = dog_name
+  end
+  def name
+    @the_dog_name
+  end
+  def breed=(dog_breed)
+    @the_dog_breed = dog_breed
+  end
+  def breed
+    @the_dog_breed
+  end
 end
